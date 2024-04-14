7607b3a3fb2780b559c2c17b69b0fe4da8f3eb53
benever <thetakky@mail.ru>
2024-04-14 14:13:03 +0300

    first commit

---
+++ src/task/oop/StreamTask.java
@@ -0,0 +1,13 @@
+package task.oop;
+
+import java.util.stream.LongStream;
+
+public class StreamTask {
+    public static void main(String[] args) {
+        System.out.println(getArithmeticProgressionSum(10000000 ,1000000000));
+    }
+    public static long getArithmeticProgressionSum(int a, int b){
+        return LongStream.range(a, b).parallel().sum();
+        // или LongStream.range(a, b).parallel().reduce(0,Long::sum);
+    }
+}
