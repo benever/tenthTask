28d06472ef3bcd0422c68eb7e0b9ddd1e3781c7a
benever <thetakky@mail.ru>
2024-04-14 16:42:11 +0300

    delete commend

--- src/task/oop/StreamTask.java
+++ src/task/oop/StreamTask.java
@@ -5,9 +5,9 @@ import java.util.stream.LongStream;
 public class StreamTask {
     public static void main(String[] args) {
         System.out.println(getArithmeticProgressionSum(10000000 ,1000000000));
+
     }
     public static long getArithmeticProgressionSum(int a, int b){
         return LongStream.range(a, b).parallel().sum();
-        // или LongStream.range(a, b).parallel().reduce(0,Long::sum);
     }
 }
